3dfe82398af30c82cd0c47e14d432e59194d2262
Ole Christensen <olenybchr@protonmail.com>
2024-10-17 00:13:14 +0200

    add base hrefs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ojnyb.github.io",
-  base: "/",
+  base: "/howdie",
   integrations: [mdx(), sitemap()],
 });
