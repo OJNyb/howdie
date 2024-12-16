d82aa6537936ac97360166bb7440e211292469ea
Ole Christensen <olenybchr@protonmail.com>
2024-12-16 19:56:32 +0100

    Fix some minor bugs, add more descriptive site titles, add site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Howdie Mickoski";
-export const SITE_DESCRIPTION = "";
+export const SITE_DESCRIPTION =
+  "Explore Howard Miskoski's insights on reality, truth, and exiting Plato's Cave. Discover his books, join discussions, and watch videos challenging conventional narratives.";
