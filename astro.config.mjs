916e7c439a32be63ca373ab84f66d47a105ebc41
Ole Christensen <olenybchr@protonmail.com>
2024-12-16 19:59:43 +0100

    Update site url in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ojnyb.github.io",
+  site: "https://howdiemickoski.com/",
   integrations: [mdx(), sitemap()],
 });
